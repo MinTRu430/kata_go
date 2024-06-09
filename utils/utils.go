ecd422e163ba0d1180450cbdf7fe6fb8bbdcf9ab
Tim <skidrow430@gmail.com>
2024-06-09 15:50:32 +0300

    second commit

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strconv"
+import (
+	"regexp"
+	"strconv"
+)
 
 func IsDigit(s string) (int, bool) {
 	num, err := strconv.Atoi(s)
@@ -13,7 +16,13 @@ func IsDigit(s string) (int, bool) {
 	return num, true
 }
 
+var romanNumeralPattern = regexp.MustCompile("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")
+
 func IsRomanDigit(s string) (int, bool) {
+	if !romanNumeralPattern.MatchString(s) {
+		panic("паника неправильный ввод римских символов")
+	}
+
 	num := RomanToInt(s)
 	if num < 1 || num > 10 {
 		panic("Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более.")
